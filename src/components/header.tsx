8e9850504515031c030a01203a9955199705c8af
Felipe Carvalho <felipe_pc7@hotmail.com>
2024-09-21 14:36:59 -0300

    create header buttons

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,32 @@
+import { useParams } from "react-router-dom";
 import Logo from "../assets/logo.png";
+import { Button } from "./ui/button";
+import { Copy, Plus } from "lucide-react";
 
 export function Header() {
+	const { shortId } = useParams();
+
 	return (
-		<header className="h-20 px-20 flex items-center">
-			<img className="w-28" src={Logo} alt="insight logos" />
-			<h1 className="font-bold text-3xl">
-				In<span className="text-sky-500">.</span>Sight
-			</h1>
+		<header className="h-20 px-20 flex justify-between items-center">
+			<div className="flex items-center gap-3">
+				<img className="w-28" src={Logo} alt="insight logos" />
+				<h1 className="font-bold text-3xl">
+					In<span className="text-sky-500">.</span>Sight
+				</h1>
+			</div>
+
+			{shortId && (
+				<div className="flex items-center gap-3">
+					<Button variant="secondary">
+						<Copy className="size-5" />
+						{shortId}
+					</Button>
+					<Button>
+						<Plus className="size-5" />
+						Nova sala
+					</Button>
+				</div>
+			)}
 		</header>
 	);
 }
